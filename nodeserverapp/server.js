3008ddf5bc64ac461001f8756397da216ed8fb00
Sammed Ravanavar <samravan@publicisgroupe.net>
2019-04-25 16:20:37 +0530

    node

---
+++ nodeserverapp/server.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var app = express();
+var mongodb = require('mongodb');
+var cors = require('cors');
+var bp = require('body-parser');
+var router = express.Router(); //Server side router
+
+
+app.use(cors())
+app.use(bp.json())
+
+let url = "mongodb://127.0.0.1:27017";
+let MongoClient = mongodb.MongoClient;
+router.route('/products').get((req,res)=>{
+    //code to get data from mongodb
+    //connect to mongodb server
+    //use MongoClient --> mongodb
+    //get the records
+    //pass the data in json
+
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    MongoClient.connect(url,{useNewUrlParser:true},(err,db)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            let collection = db.db("SapientDB").collection("productlist");
+            collection.find({}).toArray((err,result)=>{
+                if(result.length){
+                    res.json(result); //sending the json response
+                }
+            })
+        }
+    })
+})
+
+router.route('/addproduct').post((req,res)=>{
+    console.log(req.body)
+    MongoClient.connect(url,{useNewUrlParser:true},(err,db)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            let collection = db.db("SapientDB").collection("productlist");
+            collection.insertOne(req.body).then((err,result)=>{
+                if(err){
+                    res.send(err)
+                }
+                else{
+                    res.send(result)
+                }
+            })
+        }
+    })
+})
+
+router.route('/deleteproduct').post((req,res)=>{
+    console.log(req.body)
+    MongoClient.connect(url,{useNewUrlParser:true},(err,db)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            let collection = db.db("SapientDB").collection("productlist");
+            collection.deleteOne({"id":req.body.theId}).then((err,result)=>{
+                if(err){
+                    res.send(err)
+                }
+                else{
+                    res.send(result)
+                }
+            })
+        }
+    })
+})
+
+router.route('/incrementlikes').post((req,res)=>{
+    console.log(req.body)
+    MongoClient.connect(url,{useNewUrlParser:true},(err,db)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            let collection = db.db("SapientDB").collection("productlist");
+            collection.findOneAndUpdate({"id":req.body.id},{$inc:{"likes":1}}).then((err,result)=>{
+                if(!err){
+                    res.send(result)
+                }
+            })
+        }
+    })
+})
+
+app.use('/',router);
+app.get('/',(req,res)=>{
+    res.send("Use /products to get the products!");
+})
+app.use((req,res)=>{
+    if(res.statusCode == 404);
+    res.send("Error !")
+});
+app.listen(5000,()=>{
+    console.log("Server running at port 5000!");
+})
